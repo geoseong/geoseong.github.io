ff5de61840ed08885e7c65bac978d2f112846036
TaeSeong Park <imf4@naver.com>
2019-06-16 00:06:14 +0900

    Updates

--- adsense.js
+++ adsense.js
@@ -1,4 +1,5 @@
-let adsbygoogle = window.adsbygoogle || []
+/* eslint-disable no-undef */
+adsbygoogle = window.adsbygoogle || []
 adsbygoogle.push({
   google_ad_client: 'ca-pub-4861235624374871',
   enable_page_level_ads: true,
