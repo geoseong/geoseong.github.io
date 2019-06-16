796ae338d439ac0267ab71ee27408ba544bf7114
TaeSeong Park <imf4@naver.com>
2019-06-16 11:43:29 +0900

    fix react-hot-loader warn message installing @hot-loader/react-dom
    https://github.com/gatsbyjs/gatsby/issues/11934#issuecomment-469046186

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,3 +44,13 @@ exports.createPages = ({ graphql, actions: { createPage } }) => {
     resolve()
   })
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom',
+    }
+  }
+}
